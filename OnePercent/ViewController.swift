9b9fc6d118971a96c8bd4952f13babdbd821258c
김혜원 <gpdnjs8151@naver.com>
2016-10-24 02:06:43 +0900

    setting

---
+++ OnePercent/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OnePercent
+//
+//  Created by 김혜원 on 2016. 10. 24..
+//  Copyright © 2016년 김혜원. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'OnePercent' do
+  # Comment this line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for OnePercent
+
+  target 'OnePercentTests' do
+pod 'SwiftyTimer'
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'OnePercentUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
