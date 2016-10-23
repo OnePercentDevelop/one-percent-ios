796541bd9f89fd4286fc093441da65cd85c06792
김혜원 <gpdnjs8151@naver.com>
2016-10-24 02:56:20 +0900

    타이머 구현

--- OnePercent/ViewController.swift
+++ OnePercent/ViewController.swift
@@ -7,9 +7,36 @@
 //
 
 import UIKit
+import SwiftyTimer
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    var timer: Timer?
+
+    fileprivate func startTimer() {
+        if timer != nil {
+            stopTimer()
+        }
+        
+        self.updateTodayLeftTime()
+        timer = Timer.new(every: 0.5.second, updateTodayLeftTime)
+        timer?.start()
+    }
+    
+    fileprivate func updateTodayLeftTime() {
+    //viewController?.todayLeftTimeSecond = interactor.todayLeftSecond
+        print("\(Date())")
+      timeLabel.text = "\(Date())"
+
+    }
+    
+    fileprivate func stopTimer() {
+        timer?.invalidate()
+        timer = nil
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +46,16 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        startTimer()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        stopTimer()
+    }
 
 }
 
