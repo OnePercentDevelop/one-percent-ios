40680e36fedcc6faf1fa37cd78d54e74e495a565
김혜원 <gpdnjs8151@naver.com>
2017-01-27 18:47:00 +0900

    투표 모델

--- OnePercent/Vote.swift
+++ OnePercent/Vote.swift
@@ -8,8 +8,21 @@
 
 import Foundation
 import RealmSwift
+import ObjectMapper
 
-class Vote: Object {
+class VoteResponse: Mappable {
+    var voteTotalResult: [Vote]?
+    
+    required init?(map: Map) {
+        
+    }
+    
+    func mapping(map: Map) {
+        voteTotalResult <- map["voteTotalResult"]
+    }
+}
+
+class Vote: Object, Mappable {
     
     dynamic var voteDate = ""
     dynamic var question = ""
@@ -23,8 +36,29 @@ class Vote: Object {
     dynamic var count4 = 0
     dynamic var entryAmount = 0
     dynamic var winnerAmount = 0
+    
     override static func primaryKey() -> String? {
         return "voteDate"
+    }
+    
+    required convenience init(map: Map) {
+        self.init()
+    }
+    
+    func mapping(map: Map) {
+        voteDate <- map["vote_date"]
+        question <- map["vote_question"]
+        ex1 <- map["ex1_value"]
+        ex2 <- map["ex2_value"]
+        ex3 <- map["ex3_value"]
+        ex4 <- map["ex4_value"]
+        count1 <- map["ex1_count"]
+        count2 <- map["ex2_count"]
+        count3 <- map["ex3_count"]
+        count4 <- map["ex4_count"]
+        entryAmount <- map["total_count"]
+        winnerAmount <- map["prize_count"]
+
     }
     
 // Specify properties to ignore (Realm won't persist these)
@@ -33,3 +67,4 @@ class Vote: Object {
 //    return []
 //  }
 }
+
