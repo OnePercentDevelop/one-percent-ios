07f945054921663ec011e1dac3fc69f6f9d89c61
김혜원 <gpdnjs8151@naver.com>
2017-03-06 17:35:25 +0900

    인증요청

---
+++ OnePercent/OnePercentSignUpAlert.swift
@@ -0,0 +1,31 @@
+//
+//  OnePercentSignUpAlert.swift
+//  OnePercent
+//
+//  Created by 김혜원 on 2017. 2. 28..
+//  Copyright © 2017년 김혜원. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+func signUpAlert(viewController: UIViewController) {
+    let alertController = UIAlertController(title: "", message: "인증이 필요한 서비스입니다ㅎㅎ", preferredStyle: UIAlertControllerStyle.alert)
+    
+    alertController.addAction(UIAlertAction(title: "로그인", style: UIAlertActionStyle.default) { (result : UIAlertAction) -> Void in
+        let vc = viewController.storyboard?.instantiateViewController(withIdentifier: "LoginViewController")
+        viewController.present(vc!, animated: true, completion: nil)
+    })
+    
+    alertController.addAction(UIAlertAction(title: "회원가입", style: UIAlertActionStyle.default) { (result : UIAlertAction) -> Void in
+        let vc = viewController.storyboard?.instantiateViewController(withIdentifier: "SignUpViewController")
+        viewController.present(vc!, animated: true, completion: nil)
+    })
+    
+    alertController.addAction(UIAlertAction(title: "cancel", style: UIAlertActionStyle.default) { (result : UIAlertAction) -> Void in
+//        viewController.voteCollectionView.deselectAllItems(animated: false)
+//        viewController.voteCollectionView.reloadData()
+    })
+    
+    viewController.present(alertController, animated: true, completion: nil)
+}
